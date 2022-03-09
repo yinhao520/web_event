49516e7fd5495ff65cfaa55b92f6700def83fe24
yinhao <625566849@qq.com>
2022-03-09 09:58:56 +0800

    完成了登录和注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,10 @@
 $(function() {
+    var timer = null;
+    $.ajaxPrefilter(function(options) {
+        options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    })
+
     $('.login-dian a').on('click', function() {
         $('.login-dian').hide();
         $('.reg').show();
@@ -7,15 +13,61 @@ $(function() {
         $('.reg').hide();
         $('.login-dian').show();
     })
+
+
+
     $('.reg form').on('submit', function(e) {
         e.preventDefault();
-        var zhanghao = $('.reg .name').val().trim();
-        var mima = $('.reg .password').val().trim();
+        $.post('/api/reguser', {
+            username: $('.reg .name').val().trim(),
+            password: $('.reg .password').val().trim()
 
-        if (/^[0-9a-zA-Z_]{6,12}$/.test(zhanghao)) {
+        }, function(res) {
+            timer = null;
+            if (res.status != 0) {
+                $('.tips').fadeTo(500, 0.7).html(res.message);
+                timer = setTimeout(function() {
+                    $('.tips').fadeOut(500);
+                }, 500)
+                return false;
+            }
+            $('.tips').fadeTo(500, 0.7).html(res.message);
+            setTimeout(function() {
+                $('.tips').fadeOut(500);
+                $('.reg-tiao').click();
+            }, 800)
 
-        }
+        })
 
     })
+    $('.login-dian form').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: {
+                username: $('.login-dian .name').val().trim(),
+                password: $('.login-dian .password').val().trim()
+            },
+            success: function(res) {
+                timer = null;
+                if (res.status != 0) {
+                    $('.tips').fadeTo(500, 0.7).html(res.message);
+                    timer = setTimeout(function() {
+                        $('.tips').fadeOut(500);
+                    }, 500)
+                    return false;
+                }
+                localStorage.setItem('token', res.token);
+                location.href = './index.html';
+            }
+
+
+
+        })
+    })
+
+
+
 
 })
